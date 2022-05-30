38fd1b337fd394085ee5d8972cd8da04e72a301d
Asier Lorenzo <asier.lorenzo@alumni.mondragon.edu>
2022-05-30 17:56:26 +0200

    Automatizo la consulta generada para obtener una función a la que informes de una fecha y te devuelva la provincia con más casos de covid y la provincia con mayor tasa de vacunación diaria en los 3 meses anteriores a dicha fecha

--- codigo.R
+++ codigo.R
@@ -1,30 +1,38 @@
 install.packages("canadacovid")
 library(canadacovid)
 library(dplyr)
+library(lubridate)
 
-reportes <- get_reports(split = c("province"))
+datos_covid_Canada <- function (fecha) {
+  reportes <- get_reports(split = c("province"))
+  
+  reportes_filter <- reportes %>%
+    filter(date > ymd(as.Date(fecha) - months(6)))
+  
+  reportes_agrupados <- reportes_filter %>%
+    select(province, date, change_cases, change_vaccinations) %>%
+    group_by(province) %>%
+    summarize(casos_medios=mean(change_cases), vacunas_medias=mean(change_vaccinations))
+  
+  provincias <- get_provinces(geo_only = TRUE)
+  
+  reportes_agrupados_ <- merge(reportes_agrupados, provincias, by.x="province", by.y="code", all.x=TRUE)
+  reportes_agrupados_$tasa_casos_medios <- reportes_agrupados_$casos_medios/reportes_agrupados_$population
+  reportes_agrupados_$tasa_vacunas_medias <- reportes_agrupados_$vacunas_medias/reportes_agrupados_$population
+  
+  Provincia_Mayor_Tasa_Casos <- reportes_agrupados_ %>%
+    filter(tasa_casos_medios==max(tasa_casos_medios)) %>%
+    select(name)
+  
+  Provincia_Mayor_Tasa_Vacunas <- reportes_agrupados_ %>%
+    filter(tasa_vacunas_medias==max(tasa_vacunas_medias)) %>%
+    select(name)
+  
+  return(print(paste("La provincia con mayor tasa de contagios es", Provincia_Mayor_Tasa_Casos, "y la provincia con mayor tasa de vacunas diarias es", Provincia_Mayor_Tasa_Vacunas)))
+}
 
-reportes_filter <- reportes %>%
-  filter(date > "2021-11-30")
+datos_covid_Canada("2021-06-30")
 
-reportes_agrupados <- reportes_filter %>%
-  select(province, date, change_cases, change_vaccinations, name) %>%
-  group_by(province) %>%
-  summarize(casos_medios=mean(change_cases), vacunas_medias=mean(change_vaccinations))
 
-provincias <- get_provinces(geo_only = TRUE)
 
-reportes_agrupados_ <- merge(reportes_agrupados, provincias, by.x="province", by.y="code", all.x=TRUE)
-reportes_agrupados_$tasa_casos_medios <- reportes_agrupados_$casos_medios/reportes_agrupados_$population
-reportes_agrupados_$tasa_vacunas_medias <- reportes_agrupados_$vacunas_medias/reportes_agrupados_$population
 
-Provincia_Mayor_Tasa_Casos <- reportes_agrupados_ %>%
-  filter(tasa_casos_medios==max(tasa_casos_medios)) %>%
-  select(name)
-
-Provincia_Mayor_Tasa_Vacunas <- reportes_agrupados_ %>%
-  filter(tasa_vacunas_medias==max(tasa_vacunas_medias)) %>%
-  select(name)
-
-print(paste("La provincia con mayor tasa de contagios es", Provincia_Mayor_Tasa_Casos))
-print(paste("La provincia con mayor tasa de vacunas diarias es", Provincia_Mayor_Tasa_Vacunas))
